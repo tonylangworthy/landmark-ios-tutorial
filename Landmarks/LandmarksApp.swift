ad878d7b3e5abed1b76616eb6a3d07db1b296f72
Tony Langworthy <langworthytech@gmail.com>
2023-03-13 21:15:43 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Tony Langworthy on 3/13/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
